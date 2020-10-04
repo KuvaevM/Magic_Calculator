c8a8073943a33f2dcf8f8d9c88f063ee0c64b509
Максим Куваев <shlyabom@gmail.com>
2020-10-04 23:20:04 +0300

    добавлены докстринги и комментарии

--- standart_libriary.py
+++ standart_libriary.py
@@ -1,10 +1,12 @@
 def memory_re_allocation(filled_size, total_size):
+    """Размер dict будет изменен, если он заполнен на две трети"""
     if filled_size > total_size*2/3:
         return 'Память перевыделилась'
     return 'Память осталась прежней'
 
 
 def most_popular_item(text):
+    """Ищется элемент, наиболее частый в строке"""
     maximum = 0
     frequent_element = ''
     for i in text:
@@ -19,6 +21,7 @@ def most_popular_item(text):
 
 
 def most_popular_word(text):
+    """Ищется слово наиболее популярное в тексте"""
     list_of_words = text.split()
     maximum = 0
     frequent_word = ''
@@ -34,18 +37,21 @@ def most_popular_word(text):
 
 
 def average_word_length(text):
+    """Расчитывается средняя длина слов"""
     list_of_words = text.split()
     summary_length = 0
     word_count = len(list_of_words)
     for i in list_of_words:
-        summary_length+=len(i)
+        summary_length += len(i)
     return summary_length/word_count
 
 
 def palindrom(alphabet, length):
+    """Ищутся все палиндромы длины не более length из букв алфавита"""
     list_of_palindroms = []
     list_of_symbols = alphabet.split()
-    for i in range(length):
+    for i in range(length):  # в список палиндромов добавляются первые половины палиндромов с чётной длиной и первые
+        # половины с центральным элементом палиндромов с нечётной длиной
         list_of_palindroms.append([])
         if i == 0 or i == 1:
             for j in range(len(list_of_symbols)):
@@ -54,16 +60,16 @@ def palindrom(alphabet, length):
             for j in list_of_palindroms[i-2]:
                 for k in list_of_palindroms[0]:
                     list_of_palindroms[i].append(j+k)
-    for i in range(length):
+    for i in range(length):  # к палиндромам добавляются недостающие зеркальные части
         for j in range(len(list_of_palindroms[i])):
-            if i%2 == 0:
+            if i % 2 == 0:
                 help_str = list_of_palindroms[i][j][:-1]
                 reverse_str = help_str[::-1]
                 list_of_palindroms[i][j] += reverse_str
             else:
                 reverse_str = list_of_palindroms[i][j][::-1]
                 list_of_palindroms[i][j] += reverse_str
-    return  list_of_palindroms
+    return list_of_palindroms
 
 
 if __name__ == '__main__':
