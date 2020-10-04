e74cc64906acd7637d6c2763903990f845f3e18b
Максим Куваев <shlyabom@gmail.com>
2020-10-04 22:17:49 +0300

    добавлена функция дла нахождения самого популярного слова

--- standart_libriary.py
+++ standart_libriary.py
@@ -18,5 +18,20 @@ def most_popular_item(text):
     return frequent_element
 
 
+def most_popular_word(text):
+    list_of_words = text.split()
+    max = 0
+    frequent_word = ''
+    for i in list_of_words:
+        counter = 0
+        for j in list_of_words:
+            if i == j:
+                counter = counter+1
+        if(max<counter):
+            max = counter
+            frequent_word = i
+    return frequent_word
+
+
 if __name__ == '__main__':
     print(most_popular_item('0rrrrrrrrrrrrrrrrrryyuyroooooo0000000000000000000000000000000000000000000ooo     '))
